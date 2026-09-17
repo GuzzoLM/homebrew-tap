3d61cf914f7cb6690cf7f28c905ca8627f7b57bf
Lucas Guzzo <lucas.guzzo@quintoandar.com>
2026-09-17 10:59:17 -0300

    Use public Donna 0.1.0 artifact

--- Casks/donna.rb
+++ Casks/donna.rb
@@ -2,7 +2,7 @@ cask "donna" do
   version "0.1.0"
   sha256 "3ff00f40d1c41caadd577b60f4535fa421ddacc1f2d2fbba5aa7450dbed131da"
 
-  url "https://github.com/GuzzoLM/donna/releases/download/v#{version}/Donna-#{version}-macos-arm64.zip"
+  url "https://raw.githubusercontent.com/GuzzoLM/donna-releases/v#{version}/Donna-#{version}-macos-arm64.zip"
   name "Donna"
   desc "Personal mission control for work"
   homepage "https://github.com/GuzzoLM/donna"
